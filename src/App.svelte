f4b104c33a4230e988147d590c196c5739a8fc8f
alaqsaka <aqsaabidin15@gmail.com>
2021-11-16 00:28:32 +0700

    Learned about inline event handlers

--- src/App.svelte
+++ src/App.svelte
@@ -24,6 +24,11 @@
 		{ name: 'Luigi', colorOfBelt: 'Brown', age: 35, id: 3 },
 	];
 
+
+	const deleteButton = (id) => {
+		// Delete the person from people
+		people = people.filter((person) => person.id != id)
+	}
 </script>
 
 <main>
@@ -33,14 +38,16 @@
 	<input type="text" bind:value={beltColour}>
 
 	<h1>Learning LOOPS!</h1>
-	<p>Section for practicing about loops</p>
+	<p>Section for practicing about loops<br/>
+	button for learning about inlinve event handlers </p>
 
 	{#each people as person (person.id)}
 		<div>
 			<h4>{person.name}</h4>
 			<p>{person.age} years old, {person.colorOfBelt} belt.</p>
+			<button on:click={() => deleteButton(person.id)}>Delete</button>
 		</div>
-	<!-- Misalkan engga ada element di dalam array -->
+	<!-- Else buat misalkan engga ada element di dalam array -->
 	{:else}
 		<p>There are no people to show</p>
 	{/each}
